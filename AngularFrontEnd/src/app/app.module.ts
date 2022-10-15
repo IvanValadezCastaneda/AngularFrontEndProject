8cca6270e85320c6497464f19f4f0f6be9a3b6a1
Ivan Valadez Castaneda <91768558+IvanValadezCastaneda@users.noreply.github.com>
2022-10-15 15:06:17 -0500

    Revert "Commit of SideNav and HomePage"
    This reverts commit 794f9aca1ec6b1eb1895efb60ae6b41c424a278c.

--- AngularFrontEnd/src/app/app.module.ts
+++ AngularFrontEnd/src/app/app.module.ts
@@ -1,32 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import { SideNavComponent } from './side-nav/side-nav.component';
-import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent,
-    SideNavComponent
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    ReactiveFormsModule,
-    FormsModule,
-    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [
-    AppComponent,
-    SideNavComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
