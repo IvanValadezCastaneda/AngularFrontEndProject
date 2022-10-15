433e8ecf9129b17fb5e4e4165e7fe2eb9504c321
Ivan Valadez Castaneda <91768558+IvanValadezCastaneda@users.noreply.github.com>
2022-10-15 15:08:36 -0500

    Commit of Rebase

--- AngularFrontEnd/src/app/app.module.ts
+++ AngularFrontEnd/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
