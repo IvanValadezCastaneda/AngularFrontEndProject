83be9150f069b6b7bd8f07196d8c912d8a3c77e2
Ivan Valadez Castaneda <91768558+IvanValadezCastaneda@users.noreply.github.com>
2022-10-22 18:43:32 -0500

    Toolbar Stylised

--- AngularFrontEnd/src/app/core/components/header/toolbar/toolbar.component.ts
+++ AngularFrontEnd/src/app/core/components/header/toolbar/toolbar.component.ts
@@ -1,10 +1,10 @@
 import { Component} from '@angular/core';
-
-
+import {MatIconModule} from '@angular/material/icon'
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
+
 })
 
 export class ToolbarComponent{
