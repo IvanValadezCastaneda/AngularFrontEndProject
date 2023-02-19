a85f0e508c863d6056189a5fa49a805c52b0dd75
Ivan Valadez Castañeda <ivanvaladezcastaneda@gmail.com>
2023-02-18 22:34:05 -0600

    Rewrite

--- AngularFrontEnd/src/app/core/components/crear-requisicion/crear-requisicion.component.ts
+++ AngularFrontEnd/src/app/core/components/crear-requisicion/crear-requisicion.component.ts
@@ -1,20 +1,12 @@
 import { Component } from '@angular/core';
 
-interface Proj {
-  value: string;
-  viewValue: string;
-}
-
 @Component({
   selector: 'app-crear-requisicion',
   templateUrl: './crear-requisicion.component.html',
+  styleUrls:['./crear-requisicion.component.css']
 })
+
 
 export class CrearRequisicionComponent{
 
-  Projects: Proj[]=[
-    { value: '0', viewValue: 'Mantenimiento'},
-    { value: '1', viewValue: 'Reparacion'},
-    { value: '3', viewValue: 'Interno'},
-  ]
 }
